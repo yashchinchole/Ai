a4520367a8b76bf08d1ca0638794fb1ef1f36034
YASH <iamyash337@gmail.com>
2025-08-16 05:42:18 +0000

    Aug 16, 2025, 11:12:18 AM

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from gitingest import ingest
+
+
 def main():
-    print("Hello from ai!")
+    # _, tree, _ = ingest(r"E:\YASH\Github\AgentHub", include_gitignored=True)
+    _, tree, _ = ingest(r"E:\YASH\Github\Ai", include_gitignored=False)
+
+    with open("ai_tree.txt", "w", encoding="utf-8") as f:
+        f.write(tree)
 
 
 if __name__ == "__main__":
